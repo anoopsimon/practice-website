61e6959a450143fad8ba104a46ebd6aaa246c30d
Anoop Simon <anoop.simon@adudev.com>
2021-07-02 15:59:06 +1000

    added fuzzy search

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+ import FuzzySearch from 'react-fuzzy'
+const list = [{
+  id: 1,
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald'
+}, {
+  id: 2,
+  title: 'The DaVinci Code',
+  author: 'Dan Brown'
+}, {
+  id: 3,
+  title: 'Angels & Demons',
+  author: 'Dan Brown'
+}];
 
 export class Movies extends Component {
   static displayName = Movies.name;
 
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0 };
+    this.state = { 
+      currentCount: 0 ,
+      title:'Movies',
+     
+    
+   };
     this.incrementCounter = this.incrementCounter.bind(this);
   }
 
@@ -15,16 +34,35 @@ export class Movies extends Component {
     });
   }
 
+   handleOnSearch = (string, results) => {
+    console.log(string, results);
+  };
+
+   handleOnHover = (result) => {
+    console.log(result);
+  };
+
+   handleOnSelect = (item) => {
+    console.log(item);
+  };
+
+   handleOnFocus = () => {
+    console.log("Focused");
+  };
   render() {
     return (
       <div>
-        <h1>Counter</h1>
-
-        <p>This is a simple example of a React component.</p>
-
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <h1>{this.state.title}</h1>
+      
+        <FuzzySearch
+      list={list}
+      keys={['author', 'title']}
+      width={430}
+      onSelect={(newSelectedItem) => {
+        // Local state setter defined elsewhere
+       // setSelectedItem(newSelectedItem)
+      }}
+    />
       </div>
     );
   }
