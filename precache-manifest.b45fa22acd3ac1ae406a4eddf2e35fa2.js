123464206de155df905f32a815533a8dc2a69bab
Anoop Simon <anoop.simon@adudev.com>
2021-07-05 16:40:01 +1000

    Updates

---
+++ precache-manifest.b45fa22acd3ac1ae406a4eddf2e35fa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317ae3d4e92287f781aa71d7fd14ccbf",
+    "url": "/practice-website/index.html"
+  },
+  {
+    "revision": "9195a6961df27d2d6387",
+    "url": "/practice-website/static/css/2.23fea534.chunk.css"
+  },
+  {
+    "revision": "f5bbcb5c3aa6c029585d",
+    "url": "/practice-website/static/css/main.df560ea0.chunk.css"
+  },
+  {
+    "revision": "9195a6961df27d2d6387",
+    "url": "/practice-website/static/js/2.f03a190a.chunk.js"
+  },
+  {
+    "revision": "9b492420950f37ac8265a88715598edb",
+    "url": "/practice-website/static/js/2.f03a190a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bbcb5c3aa6c029585d",
+    "url": "/practice-website/static/js/main.2a7c691e.chunk.js"
+  },
+  {
+    "revision": "3ad026ae9d8f7d837b1d",
+    "url": "/practice-website/static/js/runtime-main.13ca5fa3.js"
+  }
+]);
