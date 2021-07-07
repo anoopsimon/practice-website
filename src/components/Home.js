e3ae4f6fa6ced17fdca3395f928819f49c4055e9
Anoop Simon <anoop.simon@adudev.com>
2021-07-07 17:36:11 +1000

    suggestions will now be random and dynamic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button,CardDeck
@@ -24,32 +25,36 @@ export class Home extends Component {
     });
   }
 
+  shuffleMovies = (array) => array.sort(() => Math.random() - 0.5).slice(0, 3);
+
   render() {
     if(this.state.loading) return(<Spinner></Spinner>)
+
+
+    const movieSuggestions = this.shuffleMovies(this.state.movies).map((movie,key) => 
+        <div>
+    <Card key={key} style={{width:'350px',height:'400px',marginTop:'3%'}}>
+    <CardImg top src={movie.image} alt="Card image cap" />
+    <CardBody>
+      <CardTitle tag="h5">{movie.name}</CardTitle>
+      <CardSubtitle tag="h6" className="mb-2 text-muted">{movie.genre}</CardSubtitle>
+      <CardText>{movie.description}</CardText>
+      <Button className="styledButton">Buy Or Rent</Button>
+    </CardBody>
+  </Card>
+  </div>
+    
+    ) ;
     return (
       <div>
         <h1 className="title">
           MOVIES TO BUY OR RENT ON <br></br>
           BLU-RAY, DVD & DIGITAL
         </h1>
-        <CardDeck>
-        {this.state.movies.map((movie,key) => 
-        
-        <Card key={key} style={{width:'350px',height:'400px',marginTop:'3%'}}>
-        <CardImg top src={movie.image} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">{movie.name}</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">{movie.genre}</CardSubtitle>
-          <CardText>{movie.description}</CardText>
-          <Button className="styledButton">Buy Or Rent</Button>
-        </CardBody>
-      </Card>
-        
-        ) }
-
-       
+      <CardDeck>
+        {movieSuggestions}
+      </CardDeck>
      
-    </CardDeck>
       </div>
     );
   }
